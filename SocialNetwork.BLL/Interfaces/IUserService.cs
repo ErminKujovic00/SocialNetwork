7ead37cab704ec5de2ea3f63dcaee7765ad80ee8
Ermin Kujovic <ermin.kujovic@authoritypartners.com>
2023-05-26 17:15:59 +0200

    Dodan BLL i DAL slojevi i jedan trail endpoint

---
+++ SocialNetwork.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetwork.BLL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        IEnumerable<UserDTO> GetUsers();
+    }
+}
---
+++ SocialNetwork.BLL/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using SocialNetwork.BLL.Data;
+using SocialNetwork.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        IEnumerable<UserDTO> IUserService.GetUsers()
+        {
+            List<UserDTO> users = new List<UserDTO>();
+            users.Add(
+                new UserDTO()
+                {
+                    UserId = new Guid(),
+                    FirstName = "Ermin",
+                    LastName = "Kujovic",
+                    Username = "erkss",
+                    UserEmail = "erminkujovic@etf.unsa.ba"
+                }
+            );
+            users.Add(
+                new UserDTO()
+                {
+                    UserId = new Guid(),
+                    FirstName = "Brus",
+                    LastName = "Wayne",
+                    Username = "Batman",
+                    UserEmail = "Brus@brus.enterprise.com"
+                }
+           );
+            return users;
+        }
+    }
+}
